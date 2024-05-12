62812e7990ac62e8f2ce490b243315aedfc2e325
Oleg Kumogorodskyy <kyym13@gmail.com>
2024-05-12 17:51:15 +0200

    adde_POM_hw_20

--- page-objects/forms/registerPOM.ts
+++ page-objects/forms/registerPOM.ts
@@ -1,8 +1,7 @@
 import { expect, type Locator, type Page } from '@playwright/test';
-import { RegisterButton } from '../components/RegisterButton';
-
 export class RegisterPOM {
     readonly page: Page;
+    readonly signUpButton: Locator;
     readonly nameInput: Locator;
     readonly lastNameInput: Locator;
     readonly emailInput: Locator;
@@ -10,9 +9,11 @@ export class RegisterPOM {
     readonly reEnterPasswordInput: Locator;
     readonly registerButton: Locator;
     readonly errorMessage: Locator;
+    readonly clickRegister: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.signUpButton = page.getByRole('button', { name: 'Sign up' });
         this.nameInput = page.locator('#signupName');
         this.lastNameInput = page.locator('#signupLastName');
         this.emailInput = page.getByLabel('Email');
@@ -20,40 +21,81 @@ export class RegisterPOM {
         this.reEnterPasswordInput = page.getByLabel('Re-enter password');
         this.registerButton = page.getByRole('button', { name: 'Register' });
         this.errorMessage = page.getByRole('paragraph');
+        this.clickRegister = page.locator('div').filter({ hasText: /^Register$/ });
     }
 
+    async clickSignUpButton() {
+        await this.signUpButton.click();
+    }
 
     async open() {
-        const registerButton = new RegisterButton(this.page);
-        await registerButton.clickSignUpButton();
-    }
-    async fillName(name: string) {
-        await this.nameInput.fill(name);
+        await this.clickSignUpButton();
     }
 
-    async fillLastName(lastName: string) {
+    async registerValidData(name: string, lastName: string, email: string, password: string, reenterPassword: string) {
+        await this.nameInput.fill(name);
         await this.lastNameInput.fill(lastName);
+        await this.emailInput.fill(email);
+        await this.passwordInput.fill(password);
+        await this.reEnterPasswordInput.fill(reenterPassword);
+        await this.registerButton.click();
+    }
+
+    async submitRegistration() {
+        await this.clickRegister.click();
+    }
+
+    async checkNameFieldValidation(message: string) {
+        await expect(this.errorMessage).toHaveText(message);
+    }
+
+    async checkLastNameFieldValidation(message: string) {
+        await expect(this.errorMessage).toHaveText(message);
+    }
+
+    async checkEmailFieldValidation(message: string) {
+        await expect(this.errorMessage).toHaveText(message);
+    }
+
+    async checkPasswordValidation(message: string) {
+        await expect(this.errorMessage).toHaveText(message);
+    }
+
+    async checkReenterPasswordValidation(message: string) {
+        await expect(this.errorMessage).toHaveText(message);
+    }
+
+    async checkNameFieldBorderColor() {
+        await expect(this.nameInput).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    }
+
+    async checkLastNameFieldBorderColor() {
+        await expect(this.lastNameInput).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    }
+
+    async checkEmailFieldBorderColor() {
+        await expect(this.emailInput).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    }
+
+    async checkPasswordFieldBorderColor() {
+        await expect(this.passwordInput).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    }
+
+    async checkReenterPasswordFieldBorderColor() {
+        await expect(this.passwordInput).toHaveCSS('border-color', 'rgb(206, 212, 218)');
     }
 
     async fillEmail(email: string) {
         await this.emailInput.fill(email);
     }
 
-    async fillPassword(password: string) {
+    async fillPassword(password: string, reenterPassword: string) {
         await this.passwordInput.fill(password);
+        await this.reEnterPasswordInput.fill(reenterPassword);
     }
 
-    async fillReEnterPassword(password: string) {
-        await this.reEnterPasswordInput.fill(password);
+    async isRegisterButtonDisabled() {
+        await expect(this.registerButton).toBeDisabled();
     }
 
-    async submitRegistration() {
-        await this.registerButton.click();
-    }
-
-    async navigate() {
-        await this.page.goto('/');
-        await this.page.getByRole('button', { name: 'Sign up' }).click();
-    }
 }
- 
--- tests/registr.spec.ts
+++ tests/registr.spec.ts
@@ -1,25 +1,20 @@
 import { test, expect } from '@playwright/test';
-import { RegisterButton } from '../page-objects/components/RegisterButton';
 import { RegisterPOM } from '../page-objects/forms/registerPOM';
+
+
 test.describe('All tests', () => {
-  let registerButton: RegisterButton;
   let registerPOM: RegisterPOM;
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
-    registerButton = new RegisterButton(page);
     registerPOM = new RegisterPOM(page);
-  });
-  test('Successful registration with valid data', async ({ page }) => {
-     
-    await page.locator('#signupLastName').fill('Kumogorodskyy');
-    await page.getByLabel('Email').fill('kyym13+aqe6@gmail.com');
-    await page.getByLabel('Password', { exact: true }).fill('Qa12345678');
-    await page.getByLabel('Re-enter password').fill('Qa12345678');
-    await page.getByRole('button', { name: 'Register' }).click();
-    await expect(page.getByRole('heading')).toContainText('Garage');
+    await registerPOM.open();
   });
 
+  test('Successful registration with valid data', async ({ page }) => {
+    await registerPOM.registerValidData('Oleg', 'Kumogorodskyy', 'kyym13+aqe6@gmail.com', 'Qa12345678', 'Qa12345678');
+    await expect(page.getByRole('heading')).toContainText('Garage');
+  });
 
   test.afterAll('Delete user after successful registration', async ({ page }) => {
     await page.goto('/');
@@ -31,151 +26,146 @@ test.describe('All tests', () => {
     await page.getByRole('link', { name: 'Settings', exact: true }).click();
     await page.getByRole('button', { name: 'Remove my account' }).click();
     await page.getByRole('button', { name: 'Remove' }).click();
-
   });
 
-
   test.describe('Field: Name', () => {
-    test('Empty field', async ({ page }) => {
-      await page.locator('#signupName').click();
-      await page.locator('div').filter({ hasText: /^Last name$/ }).click();
-      await expect(page.getByText('Name required', { exact: true })).toBeVisible();
+    let registerPOM: RegisterPOM;
+
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/');
+      registerPOM = new RegisterPOM(page);
+      await registerPOM.open();
     });
 
-    test('Wrong data', async ({ page }) => {
-      await page.locator('#signupName').fill('123qwe');
-      await page.locator('#signupLastName').click();
-      await expect(page.locator('form')).toContainText('Name is invalid');
+    test('Empty field', async () => {
+      await registerPOM.nameInput.click();
+      await registerPOM.lastNameInput.click();
+      await registerPOM.checkNameFieldValidation('Name required');
     });
-    test('Wrong length ', async ({ page }) => {
-      await page.locator('#signupName').fill('a');
-      await page.locator('#signupLastName').click();
-      await expect(page.getByRole('paragraph')).toContainText('Name has to be from 2 to 20 characters long');
+
+    test('Wrong data', async () => {
+      await registerPOM.nameInput.fill('123qwe');
+      await registerPOM.lastNameInput.click();
+      await registerPOM.checkNameFieldValidation('Name is invalid');
     });
-    test('Border color red', async ({ page }) => {
-      await page.locator('#signupName').fill('a');
-      await page.locator('#signupLastName').click();
-      await expect(page.getByRole('paragraph')).toContainText('Name has to be from 2 to 20 characters long');
-      await expect(page.getByRole('paragraph')).toHaveCSS('color', 'rgb(220, 53, 69)')
+
+    test('Wrong length ', async () => {
+      await registerPOM.nameInput.fill('a');
+      await registerPOM.lastNameInput.click();
+      await registerPOM.checkNameFieldValidation('Name has to be from 2 to 20 characters long');
+    });
+
+    test('Border color red', async () => {
+      await registerPOM.nameInput.fill('a');
+      await registerPOM.lastNameInput.click();
+      await registerPOM.checkNameFieldBorderColor();
     });
   });
 
   test.describe('Field: Last Name', () => {
-    test('Empty field', async ({ page }) => {
-      await page.locator('#signupLastName').click();
-      await page.getByLabel('Name').click();
-      await expect(page.locator('form')).toContainText('Last name required');
+    let registerPOM: RegisterPOM;
+
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/');
+      registerPOM = new RegisterPOM(page);
+      await registerPOM.open();
     });
 
-    test('Wrong data last name', async ({ page }) => {
-      await page.locator('#signupLastName').fill('qwe123');
-      await page.getByLabel('Name').click();
-      await expect(page.locator('form')).toContainText('Last name is invalid');
+
+    test('Empty field', async () => {
+      await registerPOM.lastNameInput.click();
+      await registerPOM.nameInput.click();
+      await registerPOM.checkLastNameFieldValidation('Last name required');
     });
-    test('Wrong length last name ', async ({ page }) => {
-      await page.locator('#signupLastName').fill('q');
-      await page.getByLabel('Name').click();
-      await expect(page.locator('form')).toContainText('Last name has to be from 2 to 20 characters long');
+
+    test('Wrong data last name', async () => {
+      await registerPOM.lastNameInput.fill('qwe123');
+      await registerPOM.nameInput.click();
+      await registerPOM.checkLastNameFieldValidation('Last name is invalid');
     });
-    test('Border color red last name', async ({ page }) => {
-      await page.locator('#signupLastName').fill('q');
-      await page.getByLabel('Name').click();
-      await expect(page.locator('form')).toContainText('Last name has to be from 2 to 20 characters long');
-      await expect(page.locator('form')).toHaveCSS('color', 'rgb(55, 58, 60)')
+
+    test('Wrong length last name ', async () => {
+      await registerPOM.lastNameInput.fill('q');
+      await registerPOM.nameInput.click();
+      await registerPOM.checkLastNameFieldValidation('Last name has to be from 2 to 20 characters long');
+    });
+
+    test('Border color red last name', async () => {
+      await registerPOM.lastNameInput.fill('q');
+      await registerPOM.nameInput.click();
+      await registerPOM.checkLastNameFieldBorderColor();
     });
   });
+
   test.describe('Field: Email', () => {
-    test('Wrong data Email', async ({ page }) => {
+    let registerPOM: RegisterPOM;
 
-      await page.getByLabel('Name').fill('qwe@');
-      await page.getByLabel('Password', { exact: true }).click();
-
-      await expect(page.locator('form')).toContainText('Email is incorrect');
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/');
+      registerPOM = new RegisterPOM(page);
+      await registerPOM.open();
     });
 
 
-    test('For empty field Email', async ({ page }) => {
-      await page.getByLabel('Name').click();
-      await page.getByLabel('Password', { exact: true }).click();
-
-      await expect(page.locator('form')).toContainText('Email required');
-    });
-    test('Border color red Email', async ({ page }) => {
-      await page.getByLabel('Name').click();
-      await page.getByLabel('Password', { exact: true }).click();
-
-      await expect(page.locator('form')).toContainText('Email required');
-      await expect(page.locator('form')).toHaveCSS('color', 'rgb(55, 58, 60)')
-    });
-  });
-  test.describe('Field: Password', () => {
-    test('Wrong data', async ({ page }) => {
-
-      await page.getByLabel('Password', { exact: true }).fill('q');
-      await page.getByLabel('Re-enter password').click();
-
-      await expect(page.locator('form')).toContainText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
+    test('Wrong data Email', async () => {
+      await registerPOM.fillEmail('qwe@');
+      await registerPOM.passwordInput.click();
+      await registerPOM.checkEmailFieldValidation('Email is incorrect');
     });
 
-    test('For empty field Password', async ({ page }) => {
-      await page.getByLabel('Password', { exact: true }).click();
-      await page.getByLabel('Re-enter password').click();
-
-      await expect(page.locator('form')).toContainText('Password required');
+    test('For empty field Email', async () => {
+      await registerPOM.emailInput.click();
+      await registerPOM.passwordInput.click(); 
+      await registerPOM.checkEmailFieldValidation('Email required');
     });
-    test('Border color red Password', async ({ page }) => {
-      await page.getByLabel('Password', { exact: true }).click();
-      await page.getByLabel('Re-enter password').click();
 
-      await expect(page.locator('form')).toContainText('Password required');
-      await expect(page.locator('form')).toHaveCSS('color', 'rgb(55, 58, 60)')
+    test('Border color red Email', async () => {
+      await registerPOM.emailInput.click();
+      await registerPOM.passwordInput.click(); 
+      await registerPOM.checkEmailFieldValidation('Email required');
+      await registerPOM.checkEmailFieldBorderColor();
     });
   });
   test.describe('Field: Re-enter Password', () => {
-    test('Wrong data', async ({ page }) => {
+    let registerPOM: RegisterPOM;
 
-      await page.getByLabel('Password', { exact: true }).fill('Qa12345678');
-
-      await page.getByLabel('Re-enter password').fill('Qawsed1234');
-      await page.locator('div').filter({ hasText: /^Register$/ }).click();
-
-      await expect(page.getByRole('paragraph')).toContainText('Passwords do not match');
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/');
+      registerPOM = new RegisterPOM(page);
+      await registerPOM.open();
+    });
+    test('Wrong data', async () => {
+      await registerPOM.fillPassword('Qa12345678', 'Qawsed1234');
+      await registerPOM.submitRegistration();
+      await registerPOM.checkReenterPasswordValidation('Passwords do not match');
     });
 
-    test('For empty field Re-enter Password', async ({ page }) => {
-
-      await page.getByLabel('Password', { exact: true }).fill('Qa12345678');
-      await page.getByLabel('Re-enter password').click();
-      await page.locator('div').filter({ hasText: /^Register$/ }).click();
-
-      await expect(page.getByRole('paragraph')).toContainText('Re-enter password required');
+    test('For empty field Re-enter Password', async () => {
+      await registerPOM.fillPassword('Qa12345678', '');
+      await registerPOM.submitRegistration();
+      await registerPOM.checkReenterPasswordValidation('Re-enter password required');
     });
-    test('Border color red Re-enter Password', async ({ page }) => {
 
-      await page.getByLabel('Password', { exact: true }).fill('Qa12345678');
-      await page.getByLabel('Re-enter password').click();
-      await page.locator('div').filter({ hasText: /^Register$/ }).click();
-
-      await expect(page.getByRole('paragraph')).toContainText('Re-enter password required');
-      await expect(page.getByRole('paragraph')).toHaveCSS('color', 'rgb(220, 53, 69)')
+    test('Border color red Re-enter Password', async () => {
+      await registerPOM.fillPassword('Qa12345678', '');
+      await registerPOM.submitRegistration();
+      await registerPOM.checkReenterPasswordValidation('Re-enter password required');
+      await registerPOM.checkReenterPasswordFieldBorderColor();
     });
   });
+  // test.describe('Button "Register"', () => {
 
-  test.describe('Button "Register"', () => {
-    test('The button is disabled if data incorrect', async ({ page }) => {
-     
-      await page.locator('#signupName').fill('Oleg');
+  //   let registerPOM: RegisterPOM;
 
-      await page.locator('#signupLastName').fill('Kumogorodskyy');
+  //   test.beforeEach(async ({ page }) => {
+  //     await page.goto('/');
+  //     registerPOM = new RegisterPOM(page);
+  //     await registerPOM.open();
+  //   });
 
-      await page.getByLabel('Name').fill('kyym13+aqe6@gmail.com');
-
-      await page.getByLabel('Password', { exact: true }).fill('Qa12345678');
-
-      await page.getByLabel('Re-enter password').fill('Qa12345679');
-      const registerButton = page.getByRole('button', { name: 'Register' });
-      await expect(registerButton).toBeDisabled();
-    });
-  });
-
-});
+  //   test('The button is disabled if data incorrect', async () => {
+  //     await registerPOM.registerValidData('Oleg', 'Kumogorodskyy', 'kyym13+aqe6@gmail.com', 'Qa12345678', 'Qa12345679');
+  //     await registerPOM.isRegisterButtonDisabled();
+  //   });
+  // })
+})
